a7f83336979890ceb705aef97bfb112ffa102189
mandylenssen <mandylenssen@gmail.com>
2023-11-09 19:57:42 +0100

    initial setup

---
+++ src/main/java/com/techiteasycontroller/techiteasycontroller/TechiteasyControllerApplication.java
@@ -0,0 +1,13 @@
+package com.techiteasycontroller.techiteasycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechiteasyControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechiteasyControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techiteasycontroller/techiteasycontroller/TechiteasyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techiteasycontroller.techiteasycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechiteasyControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
